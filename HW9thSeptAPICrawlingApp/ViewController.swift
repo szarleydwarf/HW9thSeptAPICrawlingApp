4a7a2ea18f63af2a0c9bf37eee3236aa0455a338
RJ <r.j.chomik@gmail.com>
2020-09-09 17:05:10 +0100

    First commit of app with basic view

---
+++ HW9thSeptAPICrawlingApp/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  HW9thSeptAPICrawlingApp
+//
+//  Created by The App Experts on 09/09/2020.
+//  Copyright © 2020 The App Experts. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
