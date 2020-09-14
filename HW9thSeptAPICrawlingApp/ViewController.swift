5fe3c5a59857aa80eef026e0b20058b5a9fe18bd
RJ <r.j.chomik@gmail.com>
2020-09-14 14:12:57 +0100

    crawler work with strings, urls, ints, arrays, dictionaries. need to figure out bools

--- HW9thSeptAPICrawlingApp/ViewController.swift
+++ HW9thSeptAPICrawlingApp/ViewController.swift
@@ -10,18 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
-    var urlString:String="https://pokeapi.co/api/v2"
+    var urlString:String?="https://pokeapi.co/api/v2"
     var dictionary:[String:Any] = [:]
+    var array:[Any] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.tableView.dataSource = self
         self.tableView.delegate = self
-        if let url = URL(string: self.urlString) {    
+        if let urlString = self.urlString, urlString.contains("https://"), let url = URL(string: urlString) {
             URLSession.shared.dataTask(with: url) { (data, respons, error) in
                 guard let data = data else {return}
-                guard let jsonObject = try? JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] else {return}
-                self.dictionary = jsonObject
+                guard let jsonObject = try? JSONSerialization.jsonObject(with: data, options: []) else {return}
+                if let dictionary = jsonObject as? [String:Any]{
+                    self.dictionary = dictionary
+                } else if let array = jsonObject as? [Any] {
+                    self.array = array
+                }
                 DispatchQueue.main.async {
                     self.tableView.reloadData()
                 }
@@ -31,40 +36,77 @@ class ViewController: UIViewController {
     
     func getKeyValue(index:Int)->(key:String, value:Any?) {
         let keys = Array(self.dictionary.keys)
-        let key = keys[index]
-        let value = self.dictionary[key]
+        let key:String
+        let value:Any
+        if self.array.count > 0 {
+            key = "\(index)"
+            value = self.array[index]
+        } else {
+            key = keys[index]
+            value = self.dictionary[key] as Any
+        }
         return (key:key, value:value)
     }
 }
 
 extension ViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int  {
+        if self.array.count > 0 {
+            return self.array.count
+        }
         return self.dictionary.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "cell")
-        let tuple = getKeyValue(index: indexPath.row)
         cell.textLabel?.textColor = .gray
-        cell.textLabel?.text = tuple.key
         cell.detailTextLabel?.textColor = .blue
-        if let value = tuple.value {
-            cell.detailTextLabel?.text = "\(value)"
+        var isClickable = true
+        if self.array.count > 0 {
+            cell.textLabel?.text = "Element #\(indexPath.row)"
+        } else {
+            let tuple = getKeyValue(index: indexPath.row)
+            cell.textLabel?.text = tuple.key
+            if let value = tuple.value {
+                if let dictValue = value as? [String: Any]{
+                    cell.detailTextLabel?.text = "Dictionary with #\(dictValue.count) elements"
+                } else if let arrayValue = value as? [[String : Any]]{
+                    cell.detailTextLabel?.text = "Array with #\(arrayValue.count) elements"
+                } else if let value = value as? String {
+                    cell.detailTextLabel?.text = "\(value)"
+                    if !value.contains("https"){
+                        isClickable = false
+                    }
+                } else if let value = value as? Int {
+                    cell.detailTextLabel?.text = "\(value)"
+                    isClickable = false
+                } else {
+                    cell.detailTextLabel?.text = "nil"
+                    isClickable = false
+                }
+            }
         }
+        
+        cell.isUserInteractionEnabled = isClickable
         return cell
     }
-    
-    
 }
 
 extension ViewController:UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
         let viewController = storyboard.instantiateViewController(identifier: "ViewController") as! ViewController
-        if let value = self.getKeyValue(index: indexPath.row).value {
+        let value = self.getKeyValue(index: indexPath.row).value
+        if let value = value as? String {
             viewController.urlString = "\(value)"
+        } else if let dictValue = value as? [String:Any] {
+            viewController.dictionary = dictValue
+            viewController.urlString = nil
+        } else if let arrValue = value as? [Any] {
+            viewController.array = arrValue
+            viewController.urlString = nil
         }
-        
+        self.tableView.reloadData()
         self.navigationController?.pushViewController(viewController, animated: true)
     }
 }
